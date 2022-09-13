a993f6d685d3f19353a4c937b74c20d160651567
FarrelAN <farrelathaillah97@gmail.com>
2022-09-13 17:49:46 +0700

    Update CS1 Final

--- MFarrelAthaillahNugrohoJSleepMN/JSleep.java
+++ MFarrelAthaillahNugrohoJSleepMN/JSleep.java
@@ -1,12 +1,6 @@
 package MFarrelAthaillahNugrohoJSleepMN;
 
 
-/**
- * Write a description of class JSleep here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class JSleep
 {   
     public static void main(String[] args) {
