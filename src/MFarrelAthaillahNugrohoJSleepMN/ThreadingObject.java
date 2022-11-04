bee4b23936f1cc1b9d8e8c27a618d0f213c9f069
FarrelAN <farrelathaillah97@gmail.com>
2022-11-04 22:59:52 +0700

    pt modul 6

--- src/MFarrelAthaillahNugrohoJSleepMN/ThreadingObject.java
+++ src/MFarrelAthaillahNugrohoJSleepMN/ThreadingObject.java
@@ -8,7 +8,7 @@ public class ThreadingObject extends Thread {
 
     @Override
     public void run() {
-        System.out.println("Thread " + Thread.currentThread().getName() + " sedang berjalan");
-        System.out.println("Angka Id Thread " + Thread.currentThread().getId());
+        System.out.println(Thread.currentThread().getName() + " sedang berjalan");
+        System.out.println("Angka Id dari " + Thread.currentThread().getId());
     }
 }
